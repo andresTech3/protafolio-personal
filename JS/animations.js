be85e749c4efeeb997baa397cc90f014bdf96999
andres <andresnarvaez304@gmail.com>
2021-12-07 16:56:24 -0500

    cambio de diseño en el index

--- JS/animations.js
+++ JS/animations.js
@@ -1,33 +1,3 @@
-// inicio
-
-const ContentMedia = document.querySelectorAll(".rounded-pill")
-
-
-const qm = matchMedia("(max-width : 600px)")
-
-const cambioContent = (n1,n2,n3,reClass, addClass)=>{
-    ContentMedia[n1].classList.remove(reClass);
-    ContentMedia[n1].classList.add(addClass);
-    ContentMedia[n2].classList.remove(reClass);
-    ContentMedia[n2].classList.add(addClass);
-    ContentMedia[n3].classList.remove(reClass);
-    ContentMedia[n3].classList.add(addClass);
-}
-
-const changeSize = () =>{
-    if(qm.matches){
-        cambioContent(0,1,2,"w-50", "w-100")
-    }
-    else{
-        cambioContent(0,1,2,"w-100","w-50")
-    }
-}
-
-qm.addEventListener("change", changeSize);
-qm.addEventListener("DOMContentLoaded", changeSize);
-
-
-
 
 
 // animaciones en el index
@@ -39,5 +9,4 @@ window.addEventListener("scroll", ()=>{
     if(btn_IndexHeigth <= WindowsHeigth){
         ContentMedia.classList.add("transformUp")
     }
-    
 })
